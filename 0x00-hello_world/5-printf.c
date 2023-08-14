df2fd4fd8b8a04e0d3f13365b0be13a4b7789a5f
astawus <astawusalemayehu@gmail.com>
2023-08-14 15:11:26 -0700

    5-printf.c betty

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A c programm for 5-printf.c
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art, \n")
+	return (0);
+}
